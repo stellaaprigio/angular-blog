e4c4685a323db39f8abe080a3a866d61dbc0e44e
stellaaprigio <stellaaprigio@gmail.com>
2022-12-26 23:46:04 -0300

    estilizando o projeto

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Conheça Todos os Tecidos de Alfaiataria e Invista nessa Moda",
+    "description": "As peças em alfaiataria sempre estiveram presentes em diferentes épocas na história da moda, ganhando muita presença atualmente, quando as roupas de alta costura deixaram os escritórios e se tornaram verdadeiros ícones da moda em diversos lugares!",
+    "photoCover":"https://sacadaonline.com.br/wp-content/uploads/2022/11/01038148_0149_1-BLAZER-CROPPED-CHINO-XADREZ.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Look do Dia: Velvet Baby!",
+    "description": "Velvet baby! Gente e o tanto que amei esse vestido em veludo molhado que comprei na  @barneysny do @jc_obando ! Foi paixão à primeira vista pela cor, modelagem e tecido! Sei que muitas pessoas não gostam, mas eu acho lindo e chique, ainda mais para uma festa bacana como um casamento. Aprovado?! Ahhh recebi muitos elogios pelo cabelo e make, e claro que eu não poderia deixar de falar desse wet hair que fiz com @silasjrlima ! Amei amei amei! Quero sair com cabelo molhado todos os dias kkk! E a make maravilhosa foi feita pela super fofa @liviaeidmakeup !",
+    "photoCover":"https://www.blogdamariah.com.br/wp-content/uploads/2018/04/blogdamariah4.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Como inovar nos looks",
+    "description": "Faltou dinheiro ou imaginação na hora de inovar no look? Às vezes, é preciso saber se reinventar. E isso não quer dizer jogar tudo fora ou começar do zero. Significa fazer o melhor com o que você tem em mãos, tomar novos caminhos, ter novas ideias, ousar. Acontece a mesma coisa com a forma que você se veste. É possível inovar no look sem ter que renovar todo o seu guarda-roupa. Veja as nossas dicas!",
+    "photoCover":"https://i0.wp.com/carolsisson.com/wp-content/uploads/2021/12/27ea060c6bde6d96865766b0bd1bfa21.jpeg?w=800&ssl=1"
+  },
+  {
+    "id":"4",
+    "title": "Manu Gavassi mudou o visual e nós amamos",
+    "description": "Quando você pensa em Manu Gavassi, provavelmente, vem à sua cabeça aquela menina de cabelo chanel que conquistou o Brasil mostrando toda a sua personalidade no BBB 20, certo? Depois do enorme sucesso no reality show, no qual a cantora virou referência entre as antenadas que elegeram o corte curto o queridinho da época, chegou a vez da musa mudar radicalmente e entrar 2023 com um novo visual. A morena acaba de mostrar o resultado de um alongamento capilar, compartilhado em seu perfil no Instagram há quarenta minutos e que já conta com mais de 150 mil likes, ou seja, pelo jeito as longas madeixas castanhas foram mais do que aprovadas!",
+    "photoCover":"https://s2.glbimg.com/11Pqw8xgEsW9-O3ap12tG7BCvbo=/e.glbimg.com/og/ed/f/original/2020/04/01/91609985_2516557698595904_793615947061199900_n.jpg"
   }
 ]
